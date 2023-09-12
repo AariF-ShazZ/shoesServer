931b1b7a8d35ce484ebd2be10a0d0bc46cd0974a
Aarif-shazZ <shazaarif0@gmail.com>
2023-09-12 21:37:36 +0530

    done

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,7 @@ const userSchema = mongoose.Schema({
     username: String,
     useremail: String,
     userpassword: String,
-})
+},{timestamps: true})
 
 const UserModel = mongoose.model("User", userSchema)
 module.exports = { UserModel }
