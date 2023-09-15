5b5fdf11a5918402cc03ecc98fc4a2ee788e4e1e
Aarif-shazZ <shazaarif0@gmail.com>
2023-09-15 10:16:14 +0530

    done

--- controllers/Order.controller.js
+++ controllers/Order.controller.js
@@ -5,14 +5,14 @@ const { OrderModel } = require("../models/Orders.model");
 const orderPostData = async (req, res) => {
     const data = req.body
     try {
-            const product = new OrderModel(data);
-            await product.save();
-            const result = await OrderModel.find();
-            res.status(201).json({
-                status: 'success',
-                message: 'Place order successfully',
-                data: result
-            });
+        const product = new OrderModel(data);
+        await product.save();
+        const result = await OrderModel.find();
+        res.status(201).json({
+            status: 'success',
+            message: 'Place order successfully',
+            data: result
+        });
     } catch (err) {
         console.error(err);
         res.status(500).json({
@@ -24,28 +24,20 @@ const orderPostData = async (req, res) => {
 };
 
 const orderGetData = async (req, res) => {
-    const userID = req.body.userId;
     try {
-        const result = await CartModel.find({ userID });
-        if (result.length > 0) { 
-            res.status(200).json({
-                status: 'success',
-                message: 'Cart data retrieved successfully',
-                data: result
-            });
-        } else {
-            res.status(404).json({
-                status: 'error',
-                message: "You don't have products in Cart"
-            });
-        }
+        const orders = await OrderModel.find();
+        res.status(201).json({
+            status: 'success',
+            message: 'Get orders successfully',
+            data: orders
+        });
     } catch (err) {
         res.status(500).json({
             status: 'error',
             message: 'Internal Server Error',
-            error: err.message
+            error: err
         });
     }
 };
 
-module.exports = { orderGetData,orderPostData }
+module.exports = { orderGetData, orderPostData }
