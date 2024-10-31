759c887cb4822bbc59b9b429f454af04460701d5
AariF-ShazZ <aarif568khan@gmail.com>
2024-11-01 01:13:56 +0530

    shoes Server done

--- models/Orders.model.js
+++ models/Orders.model.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose")
 const orderSchema =mongoose.Schema ({
     userID:String,
     username: String,
-    city:String,
-    state:String,
     totalProducts: Number,
-    amount: Number
+    amount: Number,
+    isPayment:{
+        default:false
+    }
 },{timestamps: true})
 
 const OrderModel = mongoose.model("Order",orderSchema)
